680e1f9ddd5d617ac09677d7187eb23efb1f431c
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-08-06 12:41:13 -0400

    Remove layout wrap in existing pages

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,8 @@
-import { Layout } from "features/ui";
-
 function Dashboard() {
 	return (
-		<Layout>
+		<>
 			<h1 style={{ margin: "0" }}>Dashboard</h1>
-		</Layout>
+		</>
 	);
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,8 @@
-import { Layout } from "features/ui";
-
 function Projects() {
 	return (
-		<Layout>
+		<>
 			<h1 style={{ margin: "0" }}>Projects</h1>
-		</Layout>
+		</>
 	);
 }
 
