aef1a4a70a9036c227ff55cfbfadf5ce60ada9d9
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-08-08 21:29:50 -0400

    add dropdown and styling to user profile tool

--- frontend/src/features/user-profile/UserProfileTool.js
+++ frontend/src/features/user-profile/UserProfileTool.js
@@ -1,8 +1,29 @@
 function UserProfileTool() {
 	return (
-		<div>
-			<h1>UserProfileTool</h1>
-		</div>
+		<>
+			<div className='dropdown'>
+				<button
+					id='profile-tool'
+					className='btn-basic'
+					data-bs-toggle='dropdown'
+				>
+					<div className='profile-icon bg-primary'>JK</div>
+				</button>
+				<ul className='dropdown-menu small-dropdown py-1'>
+					<li>
+						<a href='#' className='dropdown-item'>
+							View Profile
+						</a>
+					</li>
+					<hr className='dropdown-divider my-1 mx-2' />
+					<li>
+						<a href='#' className='dropdown-item'>
+							Logout
+						</a>
+					</li>
+				</ul>
+			</div>
+		</>
 	);
 }
 
