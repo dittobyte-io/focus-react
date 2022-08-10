c77c48dd75c52a16c32f339ebb46bb2320d48380
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-08-10 08:07:15 -0400

    Add notification tool to header

--- frontend/src/features/notifications/NotificationTool.js
+++ frontend/src/features/notifications/NotificationTool.js
@@ -1,9 +1,24 @@
+import { BsBell } from "react-icons/bs";
+
 function NotificationTool() {
 	return (
 		<div>
-			<h1>NotificationTool</h1>
+			<div className='dropdown'>
+				<button
+					id='notification-tool'
+					className='btn-basic position-relative'
+					data-bs-toggle='dropdown'
+					aria-expanded='false'
+				>
+					<BsBell />
+					<span className='badge single translate-middle bg-danger border border-light rounded-circle'>
+						<span className='notification-text'>3</span>
+					</span>
+				</button>
+			</div>
 		</div>
 	);
 }
 
 export default NotificationTool;
+// TODO: evaluate margin on txt span element vs padding on badge element
