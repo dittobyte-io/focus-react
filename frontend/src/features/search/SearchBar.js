395d1c722a36f4872aa400d710fadc60b1bd8d16
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-08-10 19:03:46 -0400

    add search feature and jsx

--- frontend/src/features/search/SearchBar.js
+++ frontend/src/features/search/SearchBar.js
@@ -1,8 +1,20 @@
+import { BsSearch } from "react-icons/bs";
+
 function SearchBar() {
 	return (
-		<div>
-			<h1>SearchBar</h1>
-		</div>
+		<>
+			<div className='input-group header-search'>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='search anything'
+					aria-label='search'
+				/>
+				<button className='btn btn-primary' type='button'>
+					<BsSearch className='search-icon' />
+				</button>
+			</div>
+		</>
 	);
 }
 
