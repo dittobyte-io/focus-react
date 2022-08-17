ad89595b078b5972996408024e55a72386437744
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-08-17 19:08:46 -0400

    Layout login page component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,18 @@
+import loginbg from "features/ui/svgs/target-bkgd.svg";
+import { Auth } from "features/auth";
+
 function Login() {
 	return (
-		<>
-			<h1>Login</h1>
-		</>
+		<div
+			id='login'
+			style={{
+				background: `url(${loginbg}) no-repeat center center`,
+				backgroundColor: "lightslategray",
+				backgroundSize: "cover",
+			}}
+		>
+			<Auth />
+		</div>
 	);
 }
 
