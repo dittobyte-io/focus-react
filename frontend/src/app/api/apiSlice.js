f8ee511655d2badcec40344e376eb22c707419c8
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-09-10 11:41:43 -0400

    begin api slice setup

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,12 +2,21 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { setCredentials, logOut } from "features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-	baseURL: "http://localhost:8000",
+	baseURL: "https://www.focuscores.com",
 	credentials: "include",
 	prepareHeaders: (headers, { getState }) => {
 		const token = getState().auth.token;
 		if (token) {
-			headers.set("authorization", `Bearer ${token}`);
+			headers.set("authorization", `Token ${token}`);
 		}
+		return headers;
 	},
 });
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+	let result = await baseQuery(args, api, extraOptions);
+
+	if (result?.error?.originalStatus == 403) {
+		const refreshResult = await baseQuery("/refresh", api, extraOptions);
+	}
+};
