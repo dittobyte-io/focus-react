a0f4768a38b5c119ee71c8a492daf1f9e79bb5f6
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-10-02 12:15:00 -0400

    Update apiSlice.js

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,22 +1,18 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-import { setCredentials, logOut } from "features/auth/authSlice";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-	baseURL: "https://www.focuscores.com",
+	baseURL: "https://focus-platform-ag.herokuapp.com",
 	credentials: "include",
 	prepareHeaders: (headers, { getState }) => {
 		const token = getState().auth.token;
 		if (token) {
-			headers.set("authorization", `Token ${token}`);
+			headers.set("authorization", `Bearer ${token}`);
 		}
 		return headers;
 	},
 });
 
-const baseQueryWithReauth = async (args, api, extraOptions) => {
-	let result = await baseQuery(args, api, extraOptions);
-
-	if (result?.error?.originalStatus == 403) {
-		const refreshResult = await baseQuery("/refresh", api, extraOptions);
-	}
-};
+export const apiSlice = createApi({
+	baseQuery: baseQuery,
+	endpoints: (builder) => ({}),
+});
