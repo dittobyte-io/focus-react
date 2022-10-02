c94409e7f405d2e79fc191c3584d100dbfaaa263
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-10-02 12:15:16 -0400

    add middleware to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -5,4 +5,7 @@ export const store = configureStore({
 	reducer: {
 		drawer: drawerReducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(apiSlice.middleware),
+	devTools: true,
 });
