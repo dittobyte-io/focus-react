d78d08001f50ad73860beae075a5359694668562
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-10-15 16:02:03 -0400

    add apiSlice reducer to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "app/api/apiSlice";
 import drawerReducer from "features/ui/side-nav/drawerSlice";
 
 export const store = configureStore({
 	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
 		drawer: drawerReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
