40b39c01a9f05867c9884a7b1a4539022f8f52e8
ArushiSharma95 <asharma@a-gassociates.com>
2022-12-20 14:30:13 -0500

    removed newsfeed data from state

--- frontend/src/features/news/NewsList.js
+++ frontend/src/features/news/NewsList.js
@@ -4,8 +4,8 @@ import { useGetNewsFeedQuery } from "./newsApiSlice";
 
 function NewsList() {
 	let feedLists=[];
-	let userId = 1; 
-	const { data, isLoading, isSuccess } =
+	let userId = 1;  //explicitly passing the userId for now (temporarily)
+	const { data, isSuccess } =
 	useGetNewsFeedQuery(userId);
 	if (isSuccess) {
 		feedLists=data.feedlist;
@@ -13,13 +13,20 @@ function NewsList() {
 	
 	return (
 		<>
-			<div className='list-group list-group-flush'>
-			{feedLists.map((feedList,i) => {
-				if(i<5){
-					return <NewsItem key={i} title={feedLists[i].feedtitle} comment={feedLists[i].comment} date={feedLists[i].created_on_date} />
-				}
-             })}
-				
+			<div className="list-group list-group-flush">
+				{feedLists &&
+				feedLists.map((feedList, i) => {
+					if (i < 5) {
+					return (
+						<NewsItem
+						key={i}
+						title={feedLists[i].feedtitle}
+						comment={feedLists[i].comment}
+						date={feedLists[i].created_on_date}
+						/>
+					)
+					} else return null;
+				})}
 			</div>
 		</>
 	);
